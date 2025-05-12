14aee94b4f5765dd261c8056fec61ea21e475552
ryan-doughty <ryan.doughty@hotmail.com>
2025-05-12 17:59:19 +0000

    Learning about java conditional statements

---
+++ conditional-statements/IfElseDemo.java
@@ -0,0 +1,13 @@
+public class IfElseDemo {
+    public static void main(String[] args) {
+        int temperature = 25;
+
+        // If-else statement checks if the temperature is below or above freezing
+        if (temperature < 32) {
+            System.out.println("It is freezing.");
+        } else {
+            System.out.println("It is not freezing.");
+        }
+    }
+}
+
