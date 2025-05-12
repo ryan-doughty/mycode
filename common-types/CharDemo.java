3c24110ecaa402692d9cac5fdaf9abe492cf8629
ryan-doughty <ryan.doughty@hotmail.com>
2025-05-12 16:01:35 +0000

    Add common types folder

---
+++ common-types/CharDemo.java
@@ -0,0 +1,10 @@
+public class CharDemo {
+    public static void main(String[] args) {
+        char letter = 'A';
+        char symbol = '!';
+
+        System.out.println("Letter: " + letter);
+        System.out.println("Symbol: " + symbol);
+    }
+}
+
