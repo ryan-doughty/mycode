78ac0146a969f0b882d0756c3b7849ac6ca6864a
ryan-doughty <ryan.doughty@hotmail.com>
2025-05-13 15:03:38 +0000

    Learning static vs public

---
+++ static-public/Battle.java
@@ -0,0 +1,21 @@
+public class Battle {
+    static void displayEnemyName() {
+        System.out.println("Bonk the Destroyer!");
+    }
+
+    static int enemyHP() {
+        return 100;
+    }
+
+    public void getReadyMessage() {
+        System.out.println("A new fighter has entered the arena");
+    }
+    public static void main(String[] args) {
+        System.out.println("Who will you fight?");
+        displayEnemyName();
+        enemyHP();
+
+        Battle fight = new Battle();
+        fight.enemyHP();
+    }
+}
