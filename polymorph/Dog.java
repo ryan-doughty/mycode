acda92862d0523d4dc4fecd57b18dc7c57d4a07c
ryan-doughty <ryan.doughty@hotmail.com>
2025-05-14 15:48:48 +0000

    Java polymorph

---
+++ polymorph/Dog.java
@@ -0,0 +1,18 @@
+// Dog inherits from Animal and is also a Pet
+public class Dog extends Animal implements Pet {
+    public Dog(String name) {
+        super(name); //this calls the constructor from animal and passes name
+    }
+
+    //this is polymorphism, we are overriding makeSound so it works uniquely for Dog objects
+    @Override
+    public void makeSound() {
+        System.out.println("Bark");
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing fetch");
+    }
+}
+
